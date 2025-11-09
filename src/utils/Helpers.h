eb61f4d8ecfadedf5b59a5f976238197024eb6c8
Leonidas <112161546+Kvazarus@users.noreply.github.com>
2025-11-09 22:25:22 +0300

    Update Helpers.h

--- src/utils/Helpers.h
+++ src/utils/Helpers.h
@@ -4,14 +4,19 @@
 #include <string>
 #include <chrono>
 #include <iomanip>
+#include <sstream>
+#include <ctime>
 
 inline std::string getCurrentDateTime() {
     std::time_t now = std::time(nullptr);
-    std::tm* localTime = std::localtime(&now);
+    std::tm localTime{};
+    localtime_s(&localTime, &now);
+
     std::ostringstream oss;
-    oss << std::put_time(localTime, "%Y-%m-%d %H:%M:%S");
+    oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
     return oss.str();
 }
+
 
 constexpr int UNCATEGORIZED_ID = 0;
 
